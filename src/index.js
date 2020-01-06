d58d543199cc8ce42846550dfd7c26ddfc6ff2ee
App Academy Student <email@email.com>
2020-01-06 13:28:21 -0500

    Commit before branching

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import "./styles/index.scss";
-const testObj = {
-  key1: "hi",
-  key2: {
-    key3: "Hello"
-  }
-};
 
-const greeting = testObj?.key2?.key3 || testObj.key1;
 window.addEventListener("DOMContentLoaded", () => {
+  // const gameWindow = document.createElement("canvas");
+  // gameWindow.innerHTML = '<h1>canvas</h1>'
+  // document.body.append(gameWindow);
+
+
   document.body.classList.add("center");
   const card = document.createElement("div");
   card.classList.add("card", "center");
-  card.innerHTML = `<h2>${greeting} World!</h2>`;
+  card.innerHTML = `<h2> WAOJFWAOJFOWAJFWAOWorld!</h2>`;
   document.body.append(card);
 });
