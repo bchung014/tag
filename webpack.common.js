351dc059c48dabfb7f9bd4b96b63fe80e44626ec
Brandon Chung <bchung014@gmail.com>
2020-02-04 13:14:35 -0500

    Add css changes

--- webpack.common.js
+++ webpack.common.js
@@ -13,61 +13,55 @@ module.exports = {
     extensions: [".js"] // if we were using React.js, we would include ".jsx"
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-optional-chaining"],
-            exclude: /node_modules/
-          } // if we were using React.js, we would include "react"
+    rules: [{
+      test: /\.js$/, // if we were using React.js, we would use \.jsx?$/
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-optional-chaining"],
+          exclude: /node_modules/
+        } // if we were using React.js, we would include "react"
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          // you can specify a publicPath here
+          // by default it uses publicPath in webpackOptions.output
+          publicPath: "../",
+          hmr: process.env.NODE_ENV === "development"
         }
       },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
-              publicPath: "../",
-              hmr: process.env.NODE_ENV === "development"
-            }
-          },
-          "css-loader",
-          "postcss-loader"
-        ]
+        "css-loader",
+        "postcss-loader"
+      ]
+    },
+    {
+      test: /\.scss/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          // you can specify a publicPath here
+          // by default it uses publicPath in webpackOptions.output
+          publicPath: "../",
+          hmr: process.env.NODE_ENV === "development"
+        }
       },
-      {
-        test: /\.scss/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
-              publicPath: "../",
-              hmr: process.env.NODE_ENV === "development"
-            }
-          },
-          "css-loader",
-          "sass-loader",
-          "postcss-loader"
-        ]
-      }
+        "css-loader",
+        "sass-loader",
+        "postcss-loader"
+      ]
+    }
     ]
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-      ignoreOrder: false // Enable to remove warnings about conflicting order
-    }),
-    require("autoprefixer")
-  ]
+  plugins: [new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // all options are optional
+    filename: "[name].css",
+    chunkFilename: "[id].css",
+    ignoreOrder: false // Enable to remove warnings about conflicting order
+  }), require("autoprefixer")]
 };
